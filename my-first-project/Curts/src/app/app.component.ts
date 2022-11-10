4d546263e925bdcb36548f772005952441af315f
Jonathas Rosa <jonathasrosa85@gmail.com>
2022-11-10 06:05:42 -0300

    Update App.components - Curts Angular

--- my-first-project/Curts/src/app/app.component.ts
+++ my-first-project/Curts/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component,DoCheck, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +6,26 @@ import { Component, OnInit } from '@angular/core';
   <app-title></app-title>
   <router-outlet></router-outlet>`
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked{
   constructor() { }
+
   ngOnInit(): void { }
+
+  ngDoCheck(): void {
+    console.log("ngDoCheck");
+  }
+  ngAfterContentInit(): void {
+    console.log("ngAfterContentInit");
+  }
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked");
+  }
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit");
+  }
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewChecked");
+  }
+ 
 } 
 
