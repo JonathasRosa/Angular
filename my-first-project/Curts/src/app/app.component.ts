5bd75e9a80cbfb4ddf8983469f20551d26a532c4
Jonathas Rosa <jonathasrosa85@gmail.com>
2022-11-16 16:48:20 -0300

    Update Curts Angular

--- my-first-project/Curts/src/app/app.component.ts
+++ my-first-project/Curts/src/app/app.component.ts
@@ -1,15 +1,38 @@
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component,DoCheck, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnInit } from '@angular/core';
+/* 
+  DoCheck,
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit
+*/
 
 @Component({
   selector: 'app-root',
   template: `
-  <app-title></app-title>
+  <app-title *ngIf="destruir"></app-title><br>
+  <button (click)="destruirComponent()">Destruir componente</button>
+  {{valor}}
+  <button (click)="adicionar()">Adicionar</button>
   <router-outlet></router-outlet>`
 })
-export class AppComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked{
+export class AppComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
+  
+  public destruir:boolean = true;
+
+  public valor: number = 1;
+  
   constructor() { }
 
+  public adicionar(): number {
+    return this.valor += 1;
+  }
+
   ngOnInit(): void { }
+
+  public destruirComponent() {
+    this.destruir = false;
+  }
 
   ngDoCheck(): void {
     console.log("ngDoCheck");
