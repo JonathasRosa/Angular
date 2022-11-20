d1270d695a56f387ba6e96b7765bd586ab5d3491
Jonathas Rosa <jonathasrosa85@gmail.com>
2022-11-20 08:02:13 -0300

    Updade Curts Angular

--- my-first-project/Curts/src/app/app.component.ts
+++ my-first-project/Curts/src/app/app.component.ts
@@ -4,8 +4,14 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-root',
   template: `
   <app-data-binding></app-data-binding>
+  <!--Diretivas de atributo-->
   <app-diretivas></app-diretivas>
-  <router-outlet></router-outlet>`
+  <!--Diretivas Estruturais
+  <app-diretivas-atributos>
+    <h1>Aulas Diretivas Atributo</h1>
+    <h3>Final da aula</h3>
+  </app-diretivas-atributos>
+  <router-outlet></router-outlet> -->`
 })
 export class AppComponent implements OnInit {
     
--- my-first-project/Curts/src/app/diretivas-atributos/diretivas-atributos.component.ts
+++ my-first-project/Curts/src/app/diretivas-atributos/diretivas-atributos.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./diretivas-atributos.component.scss']
 })
 export class DiretivasAtributosComponent implements OnInit {
+  public valor: boolean = true;
+  public height: string = "20px";
+  public backgroundColor: string = "red";
 
+  public nome: string = "";
+  public list: Array<{ nome: string; }> = [{nome: "Jonathas Rosa"}];
+  
   constructor() { }
 
   ngOnInit(): void {
+    setInterval(() => {
+      if (this.valor) {
+        this.valor = false
+      } else {
+        this.valor = true
+      }
+      if (this.height == "20px") {
+        this.height = "50px";
+        this.backgroundColor ="blue"
+      } else {
+        this.height = "20px"
+        this.backgroundColor ="red"
+      }
+    },2000)
   }
-
+  public salvar() {
+    this.list.push({nome: this.nome})
+  }
 }
