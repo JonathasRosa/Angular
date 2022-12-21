d408204ae02d33874e9e6c962f16a4cc3ac03dd7
Jonathas Rosa <jonathasrosa85@gmail.com>
2022-12-21 10:13:22 -0300

    Update Routers Angular

--- Routes/src/app/app-routing.module.ts
+++ Routes/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//PAGES
+import { HomeComponent } from './pages/home/home.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { ERROR404Component } from './pages/error404/error404.component';
 
 const routes: Routes = [
-
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'sobre', component: SobreComponent, children: [
+    { path: 'jonathas', component: SobreComponent }      
+  ]},
+  { path: '404', component: ERROR404Component },
+  { path: '**', redirectTo: '404'}
 ];
 
 @NgModule({
--- Routes/src/app/app.module.ts
+++ Routes/src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//PAGES
+import { HomeComponent } from './pages/home/home.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { ERROR404Component } from './pages/error404/error404.component';
+import { MenuComponent } from './shared/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SobreComponent,
+    ERROR404Component,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
