ebf41d2b90d1fe226d13540727231943b26382fe
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-01-18 17:57:36 -0300

    Update Teste-Unitários com Angular

--- my-second-project/teste-unitarios/src/app/shared/investiments/services/list-investiments.service.spec.ts
+++ my-second-project/teste-unitarios/src/app/shared/investiments/services/list-investiments.service.spec.ts
@@ -3,12 +3,24 @@ import { HttpClientTestingModule, HttpTestingController } from '@angular/common/
 
 import { ListInvestimentsService } from './list-investiments.service';
 import { HttpClient } from '@angular/common/http';
+import { Investiments } from '../model/investiments';
 
 describe('ListInvestimentsService', () => {
   let service: ListInvestimentsService;
   let httpTestingController: HttpTestingController;
   let httpClient: HttpClient;
 
+  const URL =
+    'https://raw.githubusercontent.com/troquatte/fake-server/main/investiments-all.json';
+  
+  const mockList: Array<Investiments> = [
+    {
+      name: 'Banco 1',
+      value: 100
+    },
+  ]
+    
+    
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
@@ -17,6 +29,10 @@ describe('ListInvestimentsService', () => {
     httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(ListInvestimentsService);
   });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  })
 
   it('should be created', () => {
     expect(service).toBeTruthy();
